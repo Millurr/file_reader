d446d60f75c58d1dd97502f4fb2165d1ab0f1077
millurr <josh.miller1994@yahoo.com>
2023-08-15 19:48:17 -0500

    Updated prints for data read entry points

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod csv_reader;
 mod postgres;
 
 fn main() {
-    // if let Err(err) = csv_reader::read_file() {
-    //     println!("error running read_file: {}", err);
-    //     process::exit(1);
-    // }
+    println!("Start csv file read...");
+    if let Err(err) = csv_reader::read_file() {
+        println!("error running read_file: {}", err);
+        process::exit(1);
+    }
 
+    println!("\nStart postgres data read...");
     if let Err(err) = postgres::query_db() {
         println!("error running query_db: {}", err);
         process::exit(1);
